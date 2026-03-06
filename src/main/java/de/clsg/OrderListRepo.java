8b022d3d75fffb8081b9c5ebe15f313e625545de
ChrisClsg <chris@clsg.de>
2026-03-06 11:15:07 +0100

    Introduce OrderListRepo class and tests

---
+++ src/main/java/de/clsg/OrderListRepo.java
@@ -0,0 +1,27 @@
+package de.clsg;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderListRepo {
+  private List<Order> orders = new ArrayList<>();
+
+  public void addOrder(Order order) {
+    orders.add(order);
+  }
+
+  public boolean removeOrderById(String id) {
+    return orders.removeIf(o -> o.id().equals(id));
+  }
+
+  public List<Order> getAll() {
+    System.out.println("######## List of all orders ########");
+    orders.forEach(o -> System.out.println(o));
+    System.out.println("######################################");
+    return orders;
+  }
+
+  public Order getById(String id) {
+    return orders.stream().filter(o -> o.id().equals(id)).findFirst().orElse(null);
+  }
+}
---
+++ src/test/java/de/clsg/OrderListRepoTest.java
@@ -0,0 +1,85 @@
+package de.clsg;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class OrderListRepoTest {
+  private OrderListRepo or;
+  private Order o1;
+  private Order o2;
+
+  @BeforeEach
+  void setUp() {
+    or = new OrderListRepo();
+    o1 = new Order(2, "P001");
+    o2 = new Order(1, "P002");
+  }
+
+  @Test
+  void getAll_returnsEmptyListInitially() {
+    assertEquals(0, or.getAll().size());
+  }
+
+  @Test
+  void addOrder_addsOrder_soGetAllContainsIt() {
+    or.addOrder(o1);
+
+    List<Order> all = or.getAll();
+    assertEquals(1, all.size());
+    assertEquals(o1, all.get(0));
+  }
+
+  @Test
+  void addOrder_keepsInsertionOrder() {
+    or.addOrder(o1);
+    or.addOrder(o2);
+
+    List<Order> all = or.getAll();
+    assertEquals(2, all.size());
+    assertEquals(o1, all.get(0));
+    assertEquals(o2, all.get(1));
+  }
+
+  @Test
+  void getById_returnsNull_whenNotFound() {
+    or.addOrder(o1);
+
+    assertNull(or.getById("does-not-exist"));
+  }
+
+  @Test
+  void getById_returnsOrder_whenFound() {
+    or.addOrder(o1);
+    or.addOrder(o2);
+
+    Order found = or.getById(o2.id());
+    assertNotNull(found);
+    assertEquals(o2, found);
+    assertEquals("P002", found.productId());
+    assertEquals(1, found.quantity());
+  }
+
+  @Test
+  void removeOrderById_returnsFalse_whenNotFound() {
+    or.addOrder(o1);
+
+    assertFalse(or.removeOrderById("nope"));
+    assertEquals(1, or.getAll().size());
+  }
+
+  @Test
+  void removeOrderById_removesOrder_andReturnsTrue_whenFound() {
+    or.addOrder(o1);
+    or.addOrder(o2);
+
+    assertTrue(or.removeOrderById(o1.id()));
+
+    assertEquals(1, or.getAll().size());
+    assertNull(or.getById(o1.id()));
+    assertNotNull(or.getById(o2.id()));
+  }
+}
