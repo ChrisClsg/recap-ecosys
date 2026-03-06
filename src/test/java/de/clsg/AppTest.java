f648bc235eaeed82dda1fd2c138e36a4b4ef7290
ChrisClsg <chris@clsg.de>
2026-03-06 10:37:43 +0100

    Use seed() in App class

--- src/test/java/de/clsg/AppTest.java
+++ src/test/java/de/clsg/AppTest.java
@@ -4,17 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
 }
